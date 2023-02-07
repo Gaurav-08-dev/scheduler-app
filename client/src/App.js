f96687d4b7593014ff351e21bd6e45d834e6ada8
gaurav-08-dev <g8111997@gmail.com>
2023-02-07 23:16:42 +0530

    1. Added Login , signup page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,27 @@
+import React from "react";
+import {BrowserRouter,Routes,Route} from "react-router-dom";
 
-import './App.css';
+import Dashboard from "./components/Dashboard";
+import BookUser from "./components/BookUser";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import Signup from "./components/Signup";
+
+import {ToastContainer} from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <div className="App">
-    <p>Hello</p>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login />}/>  {/* authentication route */}
+        <Route path='/register' element={<Signup />}/> {/* authentication route */}
+        <Route path='/dashboard' element={<Dashboard />}/>
+        <Route path='/profile/:id' element={<Profile />}/>
+        <Route path='/book/:user' element={<BookUser />}/>
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
