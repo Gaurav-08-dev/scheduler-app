fd19fee4a841110176f0df958513b7a7cea55a7d
gaurav-08-dev <g8111997@gmail.com>
2023-02-18 21:09:39 +0530

    Updated login.js component

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import handleLogin from "../utils/resource"
+import {handleLogin} from "../utils/resource"
 
 const Login = () => {
 
